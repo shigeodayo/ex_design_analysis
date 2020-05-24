acf3ad414154257baac82c1744fdfed66257ee3f
Shigeo Yoshida <shigeodayo@cyber.t.u-tokyo.ac.jp>
2020-05-24 20:12:48 +0900

    Add example programs

---
+++ src/examples/wilcoxon.py
@@ -0,0 +1,28 @@
+# %%
+"""
+# Wilcoxbon test
+"""
+
+# %%
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+
+# %%
+CSV_PATH = '../../data/examples/wilcoxon_ex_data.csv'
+ALPHA = 0.05
+NUM_OF_PARTICIPANTS = 8
+
+# %%
+data = pd.read_csv(CSV_PATH, index_col=0)
+# data
+
+# %%
+_, p = stats.wilcoxon(data['No Prediction'], data['With Predicition'])
+print('No Predicition vs With Predicition: p={:.5f}'.format(p))
+if p < ALPHA:
+    print('Significant difference')
+else:
+    print('No significant difference')
+
+# %%
